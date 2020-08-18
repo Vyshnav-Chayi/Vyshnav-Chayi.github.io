cfb4b13c63fd12b0c8759b6e583deb3fe7336c51
Vyshnav-Chayi <69447322+Vyshnav-Chayi@users.noreply.github.com>
2020-08-18 21:35:43 +0530

    Add files via upload

--- styles.js
+++ styles.js
@@ -439,7 +439,7 @@ module.exports = content.locals || {};
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Documents\Autonise AI WorkShop\startproject\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! D:\Desktop\startproject\src\styles.css */"./src/styles.css");
 
 
 /***/ })
